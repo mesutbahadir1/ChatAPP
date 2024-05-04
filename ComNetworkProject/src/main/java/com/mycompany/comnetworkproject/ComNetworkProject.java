db79a7005935effe88f3c700e9eaf906f25f5d3a
Mesut <mesutbahadir81@gmail.com>
2024-05-04 14:59:10 +0300

    second commit

--- ComNetworkProject/src/main/java/com/mycompany/comnetworkproject/ComNetworkProject.java
+++ ComNetworkProject/src/main/java/com/mycompany/comnetworkproject/ComNetworkProject.java
@@ -12,6 +12,6 @@ package com.mycompany.comnetworkproject;
 public class ComNetworkProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hi World!");
     }
 }
