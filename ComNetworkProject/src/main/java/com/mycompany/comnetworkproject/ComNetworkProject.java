352caacd3a57d20e5333588ed83133267061b458
Mesut <mesutbahadir81@gmail.com>
2024-05-04 15:54:25 +0300

    comm

--- ComNetworkProject/src/main/java/com/mycompany/comnetworkproject/ComNetworkProject.java
+++ ComNetworkProject/src/main/java/com/mycompany/comnetworkproject/ComNetworkProject.java
@@ -12,6 +12,6 @@ package com.mycompany.comnetworkproject;
 public class ComNetworkProject {
 
     public static void main(String[] args) {
-        System.out.println("hi World!");
+        System.out.println("Selam millet!");
     }
 }
