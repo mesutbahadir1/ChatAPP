74de5e8f7174f8792ad2f5d5f91b995293821011
Mesut <mesutbahadir81@gmail.com>
2024-05-08 18:45:30 +0300

    request class

---
+++ ComNetworkProject/src/main/java/com/mycompany/comnetworkproject/Request.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.comnetworkproject;
+
+/**
+ *
+ * @author mesut
+ */
+public class Request implements java.io.Serializable {
+
+    public Object o;
+    String op;
+    String sender;
+    String reciever;
+
+    public Request(String t) {
+        this.op = t;
+    }
+
+    public Request(String op, Object objContent, String sender, String reciever) {
+        this.op = op;
+        this.o = objContent;
+        this.sender = sender;
+        this.reciever = reciever;
+    }
+
+    public Request(String op, Object objContent, String sender) {
+        this.op = op;
+        this.o = objContent;
+        this.sender = sender;
+    }
+
+}
